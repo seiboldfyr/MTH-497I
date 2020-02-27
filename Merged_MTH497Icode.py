f4001ce7497c550736c09d491cbdc8290bb790fc
Sydney Wells <wellssy20@up.edu>
2020-02-27 00:34:32 -0800

    hey we have a basic regression line

--- Merged_MTH497Icode.py
+++ Merged_MTH497Icode.py
@@ -8,8 +8,6 @@ Created on Thu Feb  6 10:47:53 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import statistics as st
-from fractions import Fraction as fr
 
 #initialize variables according to assay file
 loc1 = "Assay1.xlsx"
@@ -24,13 +22,13 @@ file = -1 #set file to a random number in order to initialize it
 
 #graphs single well, all wells, and all wells for each group by color
 def assay(location):
-    print('Here are the graphs for - '+file)  
+    print('Here are the graphs for '+file)  
     #reads excel file and plots graphs according to columns
     x=pd.read_excel(location, usecols="B")
     g=pd.read_excel(location, usecols="C")
     
     #plots single well
-    plt.title('Graph for Single Well - '+file)
+    plt.title('Graph for Single Well')
     plt.xlabel('Cycle number')
     plt.ylabel('Concentration')
     plt.plot(x,g)
@@ -39,7 +37,7 @@ def assay(location):
     #reads columns C throught CT from excel file which is all wells in data
     g2=pd.read_excel(location, usecols="C:CT")
     
-    plt.title('Graph for all wells - '+file)
+    plt.title('Graph for all wells')
     plt.xlabel('Cycle number')
     plt.ylabel('Concentration')
     plt.plot(x,g2)
@@ -53,7 +51,7 @@ def assay(location):
     
     plt.xlabel('Cycle number')
     plt.ylabel('Concentration')
-    plt.title('Graph for 4 groups - '+file)
+    plt.title('Graph for 4 groups')
     plt.plot(x,y1,color='red')#labels groups according to color
     plt.plot(x,y2,color='green')
     plt.plot(x,y3,color='yellow')
@@ -67,13 +65,12 @@ def assay(location):
     #make an array of colors to play with
     colors = ["red", "yellow", "blue", "green", "orange", "purple", "pink", "teal", "goldenrod"]
     
-    plt.title('Graph for 8 concentrations - '+file)
+    plt.title('Graph for 8 concentrations')
     plt.xlabel('Cycle number')
     plt.ylabel('Concentration')
     for k in range(8):
         #indices of each column for a given concentration
-        indices = [k*3+0, k*3+1, k*3+2, k*3+24, k*3+25, k*3+26, k*3+48, 
-                   k*3+49, k*3+50, k*3+72, k*3+73, k*3+74]
+        indices = [k*3+0, k*3+1, k*3+2, k*3+24, k*3+25, k*3+26, k*3+48, k*3+49, k*3+50, k*3+72, k*3+73, k*3+74]
         plt.plot(x, nar[:, indices], color = colors[k] ) #plot each concentration 
     plt.show()
     
@@ -89,7 +86,7 @@ def assay(location):
         #take our means and stick it in our empty array
         wells[:, int(j/3): int(j/3) + 1] = meansupright 
         
-    plt.title('Graph for Triplicates - '+file)
+    plt.title('Graph for Triplicates')
     plt.xlabel('Cycle number')
     plt.ylabel('Concentration')
     plt.plot(x, wells)
@@ -98,26 +95,25 @@ def assay(location):
     print('---------------------------------------------------------------')
     return
 
-def stddev(location, concentration):
-    x=pd.read_excel(location, usecols="B")
+##build some regression lines
+def approx(location):
+    x=pd.read_excel(location, usecols= "B")
+    ##take our boring pandas array and make it a numpy array, then kick out the extra dimension
+    x1 = np.reshape(np.array(x), -1) 
+    g=pd.read_excel(location, usecols="C")
+    ##take our boring pandas array and make it a numpy array, then kick out the extra dimension
+    g2 = np.reshape(np.array(g), -1)
+    print(x1.shape)
+    print(g2.shape)
+    regline= np.poly1d(np.polyfit(x1,g2,5))##we have a regression line! 
+    ##its not very good tho
+    myline = np.linspace(1,251)
     
-    g2=pd.read_excel(location, usecols="C:CT")
-    nar = g2.to_numpy() #convert g2 to numpy array
-    con = concentration
-    df=pd.DataFrame(g2)
-    
-    #indices of each column for a given concentration
-    indices = [(con-1)*3+0, (con-1)*3+1, (con-1)*3+2, (con-1)*3+24, (con-1)*3+25, (con-1)*3+26, (con-1)*3+48, 
-                   (con-1)*3+49, (con-1)*3+50, (con-1)*3+72, (con-1)*3+73, (con-1)*3+74]
-    
-    #df.loc[0, 4:6]
-    means = df.mean()
-    print('Mean =',means)
-    plt.plot(x, nar[:, indices], color = 'red' ) #plot each concentration 
+    plt.plot(x,g)##actual graph
+    plt.plot(myline, regline(myline))##regression line 
     plt.show()
     return
-
-stddev(loc1, 6)
+approx(loc1)
 
 #will always ask for input unless user types 'stop'
 while file != 'stop':
